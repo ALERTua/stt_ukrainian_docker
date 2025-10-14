f110b6110a5b83d47eae4ae363855473d984122f
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-10-14 21:38:44 +0300

    bugfix

--- entrypoint.py
+++ entrypoint.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     demo = create_demo()
     demo.queue(
         api_open=True,
-        max_threads=concurrency_limit,
         max_size=concurrency_limit,
         default_concurrency_limit=concurrency_limit,
     )
     demo.launch(
         show_api=True,
+        max_threads=concurrency_limit,
     )
